8375948d269c29c9e2e3b12b30f7de10234fbebf
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-09-27 22:18:58 +0530

    components

---
+++ Assignment1.1/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "neutral-50":"#f9f9f9",
+        "neutral-400":"#a2a2a2",
+        "gray":"rgba(255,255,255,0.12)",
+        "neutral-950":"#0a0a0a",
+        "neutral-900":"#171717",
+        "neutral-800":"#262626",
+        "neutral-700":"#404040",
+        "neutral-600":"#525252",
+        "neutral-500":"#737373",
+        "neutral-300":"#d3d3d3",
+        "neutral-200":"#e4e4e4",
+        "neutral-100":"#f4f4f4",
+        "red-error":"#8b141a",
+        "green-success":"#00be74",
+        "card-fill":"#1618c",
+        "searchbar-fill":"#212327",
+        "blue-wash":"rgba(117,190,239,0.2)",
+        "twitter-blue":"#1d9bf0",
+        "stroke":"#1d9bf0",
+        "blhvr":"#1871CA",
+      },
+      spacing:{
+        "w-90":"20.875rem",
+        "w-98":"446px",
+        "h-4.5":"19px",
+        "w-100":"438px",
+      },
+      fontFamily:{
+        "px-regular":"Inter",
+        "chrip":"sans",
+      },
+    },
+  },
+  corePlugins:{preflight:false,}
+  
+};
+
