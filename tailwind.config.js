a69ac27fb0f2c9d92e068ea513953efaf62b1b0f
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-10-02 19:52:21 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,88 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        "neutral-50":"#f9f9f9",
-        "neutral-400":"#a2a2a2",
-        "gray":"rgba(255,255,255,0.12)",
-        "neutral-1000":"rgba(0, 0, 0, 1)",
-        "neutral-950":"#0a0a0a",
-        "neutral-900":"#171717",
-        "neutral-800":"#262626",
-        "neutral-700":"#404040",
-        "neutral-600":"#525252",
-        "neutral-500":"#737373",
-        "neutral-300":"#d3d3d3",
-        "neutral-200":"#e4e4e4",
-        "neutral-100":"#f4f4f4",
-        "red-error":"#8b141a",
-        "green-success":"#00be74",
-        "card-fill":"#1618c",
-        "searchbar-fill":"#212327",
-        "blue-wash":"rgba(117,190,239,0.2)",
-        "twitter-blue":"#1d9bf0",
-        "stroke":"#1d9bf0",
-        "blhvr":"#1871CA",
-        "secondary":"rgba(255, 255, 255, 0.6)"
+      backgroundImage:{
+        'coverImage': "url('/public/images/cover.png')",
+        'profileImage': "url('/public/images/user-avatars/user-avatar-2.png')",
+
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
       spacing:{
-        "w-90":"20.875rem",
-        "w-98":"446px",
-        "h-4.5":"19px",
-        "w-100":"438px",
-        "40.2":"162px",
-        "58":"237px",
-        "20":"593.13px",
-        "24.5":"100px",
-        "0.5":"2px",
-        "105":"455px",
-        "50":"199px",
-        "99":"367px",
-        "93":"371px",
-        "0.25":"1px",
-        "45":"189.94",
-        "70":"275px",
-        "18":"77px",
-        "85":"355px",
+        '9.5':'2.375rem'
       },
-      fontFamily:{
-        "px-regular":"Inter",
-        
-        "chrip":"sans",
+      height:{
+        "10.5":"2.625rem",
+        "15":"3.75rem",
+        "tweet":'13.75rem',
+        '70': '4.375rem',
+        '86':'22.25rem',
       },
+      width:{
+        '21':'5.375rem',
+        '70':'17.875rem',
+        '90':'20.875rem',
+        '91':'21.75rem',
+        '99':'26.625rem',
+        '100':'27.625rem',
+        'xxl':'30.125rem',
+        'xxxl':'32rem',
+        'tweet':'37.5rem',
+        '17':'4.25rem',
+      },
+      colors:{
+        "neutral-50":"#f9f9f9",
+        "neutral-100":"#f4f4f4",
+        "neutral-200":"#e4e4e4",
+        "neutral-300":"#D3D3D3",
+        "neutral-400":"#A2A2A2",
+        "neutral-500":"#737373", 
+        "neutral-600":"#525252",
+        "neutral-700":"#404040",
+        "neutral-800":"#262626",
+        "neutral-900":"#171717",
+        "neutral-950":"#0A0A0A",
+        "neutral-1000":"#000000",
+        "twitter-blue":"#1D9BF0",
+        "twitter-blue-hover":"#1871CA",
+        "twitter-blue-disabled":"#1E5D87",
+        "blue-wash":"#75BEEF33",
+        "button-stroke":"#546A7A",
+        "search-bar-fill":"#212327",
+        "card-fill":"#16181C",
+        "success":"#00BE74",
+        "error":"#8B141A",
+        "sign-up-border":"rgba(29, 155, 240, 0.24)",
+        "dob-text":"rgba(255, 255, 255, 0.60)",
+      },
+      boxShadow:{
+        "md":"0px 8px 16px 0px rgba(0, 0, 0, 0.25)",
+      },
+      fontSize:{
+        'xs':['0.75rem', {
+          'lineHeight':'0.9375rem',
+        }],
+        'sm':['0.875rem', {
+          'lineHeight': '1.0625rem',
+        }],
+        'base':[
+          '1rem', {
+            'lineHeight': '1.2rem',
+          }
+        ],
+        'xl':['1.25rem', {
+          'lineHeight': '1.5rem',
+        }],
+        '15':[
+          '0.9375rem', {
+            'lineHeight': '1.125rem'
+          }
+        ],
+      },
+      
     },
   },
-  corePlugins:{preflight:false,}
-  
+  plugins: [],
 };
-
